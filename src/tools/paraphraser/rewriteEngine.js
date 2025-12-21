3291155c7c2090b24316bb3b48587e61d4d15d02
Mueed Basit <usmansheraz789@gmail.com>
2025-12-22 01:33:00 +0500

    Fix rewrite engine import and enforce correct generateText usage

--- src/tools/paraphraser/rewriteEngine.js
+++ src/tools/paraphraser/rewriteEngine.js
@@ -1,10 +1,14 @@
-const { generateText }  = require("../../../utils/aiClient");
+const { generateText } = require("../../../utils/aiClient");
 
 function randomBetween(min, max) {
   return min + Math.random() * (max - min);
 }
 
 module.exports = async function rewrite({ prompt, temperatureRange }) {
+  if (!prompt || typeof prompt !== "string") {
+    throw new Error("Invalid prompt passed to rewrite engine");
+  }
+
   const temperature = randomBetween(
     temperatureRange[0],
     temperatureRange[1]
